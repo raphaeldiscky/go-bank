2bd42db751b742f9a632a6dc520a235c459bae15
Raphael Discky <raph.discky@gmail.com>
2024-04-21 18:42:16 +0700

    Add new table to verify email

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	db "github.com/raphaeldiscky/simple-bank/db/sqlc"
 	_ "github.com/raphaeldiscky/simple-bank/docs/statik"
 	"github.com/raphaeldiscky/simple-bank/gapi"
+	"github.com/raphaeldiscky/simple-bank/mail"
 	"github.com/raphaeldiscky/simple-bank/pb"
 	"github.com/raphaeldiscky/simple-bank/utils"
 	"github.com/raphaeldiscky/simple-bank/worker"
@@ -54,7 +55,7 @@ func main() {
 
 	taskDistributor := worker.NewRedisTaskDistributor(redisOpt)
 
-	go runTaskProcessor(redisOpt, store)
+	go runTaskProcessor(config, redisOpt, store)
 	go runGatewayServer(config, store, taskDistributor)
 	runGrpcServer(config, store, taskDistributor)
 }
@@ -72,8 +73,9 @@ func runDBMigration(migrationURL string, dbSource string) {
 	log.Info().Msg("db migrated successfully")
 }
 
-func runTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) {
-	taskProcessor := worker.NewRedisTaskProcessor(redisOpt, store)
+func runTaskProcessor(config utils.Config, redisOpt asynq.RedisClientOpt, store db.Store) {
+	mailer := mail.NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	taskProcessor := worker.NewRedisTaskProcessor(redisOpt, store, mailer)
 	log.Info().Msg("start task processor")
 	err := taskProcessor.Start()
 	if err != nil {
--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 
 	"github.com/hibiken/asynq"
+	db "github.com/raphaeldiscky/simple-bank/db/sqlc"
+	"github.com/raphaeldiscky/simple-bank/utils"
 	"github.com/rs/zerolog/log"
 )
 
@@ -52,7 +54,30 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 		return fmt.Errorf("failed to get user: %w", asynq.SkipRetry)
 	}
 
-	// TODO: send email to user
+	verifyEmail, err := processor.store.CreateVerifyEmail(ctx, db.CreateVerifyEmailParams{
+		Username:   user.Username,
+		Email:      user.Email,
+		SecretCode: utils.RandomString(32),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to create verify email: %w", err)
+	}
+
+	subject := "Welcome to Simple Bank"
+	verifyURL := fmt.Sprintf("http://simplebank.shop/verify_email?id=%d&secret_code=%s", verifyEmail.ID, verifyEmail.SecretCode)
+	content := fmt.Sprintf(`Hello %s, <br/> 
+		Thank you for registering with us! <br/> 
+		Please <a href="%s">click here</a> to verify your email address. <br/>
+		`, user.Username, verifyURL)
+
+	to := []string{user.Email}
+
+	err = processor.mailer.SendEmail(subject, content, to, nil, nil, nil)
+
+	if err != nil {
+		return fmt.Errorf("failed to send verify email: %w", err)
+	}
 
 	log.Info().Str("type", task.Type()).
 		Bytes("payload", task.Payload()).
