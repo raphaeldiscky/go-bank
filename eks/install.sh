579a869f89becf605c1139b72eafb42d3d03a0df
Raphael Discky <raph.discky@gmail.com>
2024-04-22 11:15:41 +0700

    Deploy grpc and http to eks

---
+++ eks/install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml
